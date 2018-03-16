73fa95496f3ebf0271c4ab9fe4d8a66bcd460428
Il Ung <ilungj@gmail.com>
2018-03-15 15:39:48 -1000

    Implement the system test page

--- app/client/routes.js
+++ app/client/routes.js
@@ -5,11 +5,12 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import '/imports/ui/layouts/body.html';
 
 import '/imports/ui/pages/sample.html';
+import '/imports/ui/pages/system-test/system-test.js';
 // Set up all routes in the app
 
 FlowRouter.route('/', {
   name: 'App.main',
   action() {
-    BlazeLayout.render('Layouts_body', { main: 'Pages_sample' });
+    BlazeLayout.render('Layouts_body', { main: 'Pages_systemTest' });
   },
 });
---
+++ app/imports/ui/pages/system-test/system-test.js
@@ -0,0 +1,15 @@
+import { Template } from 'meteor/templating';
+
+import './system-test.html';
+
+Template.Pages_systemTest.onCreated(function() {
+
+});
+
+Template.Pages_systemTest.helpers({
+
+});
+
+Template.Pages_systemTest.events({
+
+});
